d0bb3079aa16e2ef1f9633074ef656b3fb8ae9e7
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-02 20:42:38 +0530

    Update Merge Sort.c

--- Merge Sort.c
+++ Merge Sort.c
@@ -1,4 +1,4 @@
-
+//To Sort a given set of elements using the Merge Sort algorithm to sort a given set of elements and determine the time required to sort the elements. Repeat the experiment for different values of n, the number of elements in the list to be sorted and plot a graph of the time taken versus n. The elements can be read from a file or can be generated using the random number generator.
   
 #include <stdio.h>
 #include <time.h>
